557f06fb3e4285b7c23535129bb645b75d54b963
Helder Rodrigues Mendes <helderrodrigues37@gmail.com>
2020-10-05 19:32:52 -0300

    Adicionando cadastro individual de produto

--- src/main/java/br/com/testePratico/controller/ProdrutoController.java
+++ src/main/java/br/com/testePratico/controller/ProdrutoController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,12 @@ public class ProdrutoController {
 	ProdutoService produtoService;
 	
 	private static final String API = "/produto";
+	
+	@PostMapping()
+	public ResponseEntity<Produto> salvar(@Valid @RequestBody ProdutoDTO produtoDTO) throws URISyntaxException{
+		Produto produto = produtoService.salvar(produtoDTO);
+		return ResponseEntity.created(new URI(API + produto.getId())).body(produto);
+	}
 	
 	@GetMapping("/verificarValidade")
 	public List<Produto> verificarValidade() { //VERIFICA SE TEM ALGUM RPODUTO PERTO DE VENCER
